76f2845343dc23c7fd5a91ef844671429f8abc55
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2025-03-15 21:14:38 +0530

    Shown How a JSX component is rendered.

---
+++ project2_Exporting/src/JSXComponent.jsx
@@ -0,0 +1,9 @@
+function JSXComponent() {
+    return (
+        <div>
+            <h1>This is a JSX Component Being rendered.</h1>
+        </div>
+    );
+}
+
+export default JSXComponent;
--- project2_Exporting/src/index.js
+++ project2_Exporting/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 import { Named } from './DemoNamedExport';
 import Default, { Named1 } from './DemoExport';
 import Alias from './DemoDefaultExport';
+
 import FunctionComponent from './FunctionComponentPureReact';
+import JSXComponent from './JSXComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,5 +16,6 @@ root.render(
       <Named1 />
       <Alias />
       <FunctionComponent />
+      <JSXComponent />
    </React.StrictMode>
 );
